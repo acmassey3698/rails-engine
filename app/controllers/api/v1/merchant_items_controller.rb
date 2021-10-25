de3c657fd568ba0ddf35085f9837c5210cedcdab
Andrew Massey <acmassey@ncsu.edu>
2021-10-25 19:10:25 -0400

    Create error handling for the merchants items endpoint

--- app/controllers/api/v1/merchant_items_controller.rb
+++ app/controllers/api/v1/merchant_items_controller.rb
@@ -3,5 +3,8 @@ class Api::V1::MerchantItemsController < ApplicationController
     merchant = Merchant.find(params[:id])
     items = merchant.items
     render json: ItemSerializer.all_items(items)
+
+  rescue ActiveRecord::RecordNotFound
+    record_not_found
   end
 end
