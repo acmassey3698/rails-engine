5b900b03d18b758baaee41d2f37c7f0156fd4926
Andrew Massey <acmassey@ncsu.edu>
2021-10-26 17:50:54 -0400

    Add class methods to item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,4 +11,18 @@ class Item < ApplicationRecord
         .order(:name)
   end
 
+  def self.search_by_min(min_price)
+    Item.where('unit_price >= ?', min_price)
+        .order(:unit_price)
+  end
+
+  def self.search_by_max(max_price)
+    Item.where('unit_price <= ?', max_price)
+        .order(:unit_price)
+  end
+
+  def self.search_within_range(min_price, max_price)
+    Item.where("unit_price >= ? and unit_price <= ?", min_price, max_price)
+  end
+
 end
