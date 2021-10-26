1e4bb821ad9a7cb21c86321a2391cfa9c27a1740
Andrew Massey <acmassey@ncsu.edu>
2021-10-26 15:02:09 -0400

    Update merchant spec to include testing for the #search

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,6 +1,6 @@
 class Merchant < ApplicationRecord
   has_many :items
-
+  validates :name, presence: true
   def self.search(name)
     Merchant.where("lower(name) LIKE ?", "%#{name.downcase}%")
             .order(:name)
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -2,6 +2,21 @@ require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
   describe 'relationships' do
-    it{should have_many(:items)}
+    it { should have_many(:items) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+  end
+
+  describe 'class methods' do
+    it '#search' do
+      create_list(:merchant, 20)
+      andrew = create(:merchant, name: "Andrew")
+      andrea = create(:merchant, name: "Andrea")
+
+      expect(Merchant.search("Andre")).to eq(andrea)
+      expect(Merchant.search("Andrew")).to eq(andrew)
+    end
   end
 end
