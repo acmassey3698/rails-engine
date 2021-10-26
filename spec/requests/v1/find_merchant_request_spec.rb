726d628e28714e90fdb1cdbd2f687a844f3ee22f
Andrew Massey <acmassey@ncsu.edu>
2021-10-26 13:44:06 -0400

    Improve test for finding one merchant

--- spec/requests/v1/find_merchant_request_spec.rb
+++ spec/requests/v1/find_merchant_request_spec.rb
@@ -4,18 +4,17 @@ RSpec.describe 'find one merchant based on search criteria' do
   it 'finds on merchant when you search by name' do
     create_list(:merchant, 15)
     andrew = create(:merchant, name: "Andrew")
-    annie = create(:merchant, name: "Annie")
+    andrea = create(:merchant, name: "Andrea")
     alex = create(:merchant, name: "Alex")
 
-    get '/api/v1/merchants/find?name=Andrew'
-
+    get '/api/v1/merchants/find?name=Andre'
     expect(response).to be_successful
 
     response_body = JSON.parse(response.body, symbolize_names: true)
     merchant = response_body[:data]
 
-    expect(merchant[:id].to_i).to eq(andrew.id)
+    expect(merchant[:attributes][:name]).to eq(andrea.name)
+    expect(merchant[:id].to_i).to eq(andrea.id)
     expect(merchant[:type]).to eq("merchant")
-    expect(merchant[:attributes][:name]).to eq(andrew.name)
   end
 end
