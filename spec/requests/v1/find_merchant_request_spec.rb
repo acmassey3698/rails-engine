70a73dc574bfa11b363a5ea3c80b9639920dc5da
Andrew Massey <acmassey@ncsu.edu>
2021-10-26 14:16:46 -0400

    Improve edge case testing for finding a merchant through the search endpoint

--- spec/requests/v1/find_merchant_request_spec.rb
+++ spec/requests/v1/find_merchant_request_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'find one merchant based on search criteria' do
     andrea = create(:merchant, name: "Andrea")
     alex = create(:merchant, name: "Alex")
 
-    get '/api/v1/merchants/find?name=Andre'
+    get '/api/v1/merchants/find?name=andre'
     expect(response).to be_successful
 
     response_body = JSON.parse(response.body, symbolize_names: true)
@@ -16,5 +16,36 @@ RSpec.describe 'find one merchant based on search criteria' do
     expect(merchant[:attributes][:name]).to eq(andrea.name)
     expect(merchant[:id].to_i).to eq(andrea.id)
     expect(merchant[:type]).to eq("merchant")
+  end
+
+  it 'is case insensitive for searching merchants' do
+    create_list(:merchant, 15)
+    andrew = create(:merchant, name: "Andrew")
+    andrea = create(:merchant, name: "Andrea")
+    alex = create(:merchant, name: "Alex")
+
+    get '/api/v1/merchants/find?name=aNdRe'
+    expect(response).to be_successful
+
+    response_body = JSON.parse(response.body, symbolize_names: true)
+    merchant = response_body[:data]
+
+    expect(merchant[:attributes][:name]).to eq(andrea.name)
+    expect(merchant[:id].to_i).to eq(andrea.id)
+    expect(merchant[:type]).to eq("merchant")
+  end
+
+  it 'returns the not found error when there is no merchant that matches the search' do
+    create_list(:merchant, 15)
+
+    get '/api/v1/merchants/find?name=onwfonweoano'
+    expect(response).to_not be_successful
+    expect(response.status).to eq(404)
+
+    response_body = JSON.parse(response.body, symbolize_names: true)
+    error = response_body[:data]
+
+    expect(error[:message]).to eq("Error: Search not completed")
+    expect(error[:errors].first).to eq("no record found with id: ")
   end
 end
