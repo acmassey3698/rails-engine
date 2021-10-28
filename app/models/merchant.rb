fb563a8f0a33607a79937bb5350768f5b6dc92ce
Andrew Massey <acmassey@ncsu.edu>
2021-10-28 13:49:11 -0400

    Remove additional relationships

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,9 +1,5 @@
 class Merchant < ApplicationRecord
   has_many :items, dependent: :destroy
-  # has_many :invoice_items, through: :items
-  # has_many :invoices, dependent: :destroy
-  # has_many :transactions, through: :invoices
-  # has_many :customers, through: :invoices
 
   validates :name, presence: true
 
